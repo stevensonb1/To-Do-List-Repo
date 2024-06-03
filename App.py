2bbef69044012ce3aa35df801d2fe34f6a2d8ade
stevensonb1 <stevensonb@stu.otc.school.nz>
2024-06-04 10:24:20 +1200

    added data module
    functions for handling fetching and saving user data

---
+++ App.py
@@ -0,0 +1,38 @@
+import customtkinter
+from tkinter import font
+
+class App(customtkinter.CTk):
+    WIDTH = 700
+    HEIGHT = 500
+
+    def __init__(self):
+        super().__init__()
+
+    def load_app_menu(self):
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2,3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+
+        self.default_font = font.nametofont("TkDefaultFont")
+        self.default_font.actual()
+
+        self.username_label = customtkinter.CTkLabel(self.sidebar_frame, text="Welcome "+self.username, font=customtkinter.CTkFont(family=self.default_font, size=18, weight="normal"))
+        self.username_label.grid(row=1, column=0)
+
+        self.task_title = customtkinter.CTkLabel(self.sidebar_frame, text="Tasks", font=customtkinter.CTkFont(family=self.default_font, size = 15, weight="normal"))
+        self.task_title.grid(row=2, column=0, pady=20)
+        self.task_container = customtkinter.CTkScrollableFrame(self.sidebar_frame, width=120)
+        self.task_container.grid(row=3, column=0)
+
+        self.change_appearance_button = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark"],
+                                                                    command=self.change_appearance_mode_event)
+        self.change_appearance_button.grid(row=6, column=0, padx=20, pady=(10, 10))
+
+        self.change_appearance.set("Dark")
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
--- Data.py
+++ Data.py
@@ -1,1 +1,19 @@
 import pickle
+
+class Data():
+    def __init__(self, data):
+        self.data = data
+    
+    def save_data(filename: str, obj):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+        except Exception as err:
+            print("Error while saving data to file:", filename, "[ERROR:]", err)
+
+    def load_data(filename: str):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception as err:
+            print("Error while loading data from file:", filename, "[ERROR]:", err)
