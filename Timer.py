99fff43de50aa6af34b30774e3a2b171654e9c5e
stevensonb1 <stevensonb@stu.otc.school.nz>
2024-07-05 15:18:42 +1200

    TImer

--- Timer.py
+++ Timer.py
@@ -1,13 +1,21 @@
 import threading
-
-'''
-t = threading.Thread(target=func, args=(1,2))
-t.run # Runs function
-t.start # Runs function on different thread
-'''
+from datetime import datetime
+import time
 
 class Timer(threading.Thread):
-    def __init__(self):
+    def __init__(self, date: str = None, time: str = None, name: str = None, fn = None):
         super().__init__()
+        self.due_date = date
+        self.due_time = time
+        self.name = name
+        self.func = fn
 
-    def start_timer(self):
+    def run(self):
+        due_datetime = datetime.strptime(f'{self.due_date} {self.due_time}', "%d/%m/%Y %I:%M %p")
+
+        current_datetime = datetime.now()
+        time_remaining = (due_datetime - current_datetime).total_seconds()
+
+        if time_remaining > 0:
+            time.sleep(time_remaining)
+        self.func(self.name)
