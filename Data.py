3b8476be9a7af43dc5d6533880deb92528e06385
stevensonb1 <stevensonb@stu.otc.school.nz>
2024-08-19 13:28:23 +1200

    stores data in folder
    used to store each user file in the main folder which is unorganized, now stores in a folder named userdata

--- Data.py
+++ Data.py
@@ -1,8 +1,19 @@
 import pickle
+import os
 
 class Data():
+    DATA_FOLDER = 'Userdata'
+
     def __init__(self, username: str):
         self.username = username
+        self._ensure_data_folder_exists()
+    
+    def _ensure_data_folder_exists(self):
+        if not os.path.exists(self.DATA_FOLDER):
+            os.makedirs(self.DATA_FOLDER)
+
+    def _get_file_path(self):
+        return os.path.join(self.DATA_FOLDER, f'{self.username}_data.pkl')
 
     def template(self):
         return {
@@ -10,7 +21,7 @@ class Data():
         }
 
     def get(self):
-        if self.load() == None:
+        if self.load() is None:
             self.save(self.template())
         return self.load()
 
@@ -18,13 +29,14 @@ class Data():
         self.save(data)
 
     def save(self, object):
-        with open(f'{self.username}_data.pkl', "wb") as file:
+        with open(self._get_file_path(), "wb") as file:
             pickle.dump(object, file)
     
     def load(self):
         try:
-            with open(f'{self.username}_data.pkl', "rb") as file:
+            with open(self._get_file_path(), "rb") as file:
                 loaded_data = pickle.load(file)
                 return loaded_data
         except FileNotFoundError:
             print(f'{self.username}_data.pkl does not exist. No data found.')
+            return None
