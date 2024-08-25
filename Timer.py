d815fe69a78ffcb7c91a314be1afa389f700c409
stevensonb1 <139927019+stevensonb1@users.noreply.github.com>
2024-08-25 21:33:54 +1200

    docstrings

--- Timer.py
+++ Timer.py
@@ -1,16 +1,21 @@
 import threading
 from datetime import datetime
 import time
-import random
 
-# Can use this for Trialling of components
 class Timer(threading.Thread):
+    """
+    A timer class thats runs on a countdown to a specified due date and time.
+    It can execute a callback function when the tiemr reaches the due date.
+    """
     def __init__(self, 
         date: str = None, 
         time: str = None, 
         name: str = None, 
         fn = None
     ):
+        """
+        Initialises the timer instance.
+        """
         super().__init__()
         self.due_date = date
         self.due_time = time
@@ -19,13 +24,23 @@ class Timer(threading.Thread):
         self._stop_event = threading.Event()
 
     def update_due_date(self, date: str, time: str):
+        """
+        Updates the due date and time for the timer.
+        """
         self.due_date = date
         self.due_time = time
 
     def clean(self):
+        """
+        Stops the timer and terminates the thread.
+        """
         self._stop_event.set()
 
     def check(self):
+        """
+        Recursively checks the time remaining until the due date and time.
+        Executes the callback function if the timer expires.
+        """
         due_datetime = datetime.strptime(
             f'{self.due_date} {self.due_time}', "%d/%m/%Y %I:%M %p")
 
@@ -42,5 +57,7 @@ class Timer(threading.Thread):
                 break
 
     def run(self):
-       # initialises the recursive check
+       """
+       Starts the timer by calling the check method.
+       """
        self.check()
