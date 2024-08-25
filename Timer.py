9ce26f27503cccc4d9e7206dba41988703a4fdff
stevensonb1 <139927019+stevensonb1@users.noreply.github.com>
2024-08-25 20:36:37 +1200

    added threading and methods to update/clean

--- Timer.py
+++ Timer.py
@@ -3,29 +3,43 @@ from datetime import datetime
 import time
 import random
 
+# Can use this for Trialling of components
 class Timer(threading.Thread):
-    def __init__(self, date: str = None, time: str = None, name: str = None, fn = None):
+    def __init__(self, 
+        date: str = None, 
+        time: str = None, 
+        name: str = None, 
+        fn = None
+    ):
         super().__init__()
         self.due_date = date
         self.due_time = time
         self.name = name
         self.func = fn
+        self._stop_event = threading.Event()
 
-    def update_due_date(self, date, time):
+    def update_due_date(self, date: str, time: str):
         self.due_date = date
         self.due_time = time
 
+    def clean(self):
+        self._stop_event.set()
+
     def check(self):
-        due_datetime = datetime.strptime(f'{self.due_date} {self.due_time}', "%d/%m/%Y %I:%M %p")
+        due_datetime = datetime.strptime(
+            f'{self.due_date} {self.due_time}', "%d/%m/%Y %I:%M %p")
 
-        current_datetime = datetime.now()
-        time_remaining = (due_datetime - current_datetime).total_seconds()
+        while not self._stop_event.is_set():
+            current_datetime = datetime.now()
+            time_remaining = (due_datetime - current_datetime).total_seconds()
 
-        if time_remaining > 0:
-            time.sleep(1) # Prevents crashing
-            self.check()
-        else:
-            self.func(self.name)
+            if time_remaining > 0:
+                time.sleep(1) # Prevents crashing
+                self.check()
+            else:
+                if self.func:
+                    self.func(self.name)
+                break
 
     def run(self):
        # initialises the recursive check
