c346fc58c53567419e87fbdc501a2fa0666836b3
stevensonb1 <139927019+stevensonb1@users.noreply.github.com>
2024-08-25 20:38:35 +1200

    changes

--- Constants.py
+++ Constants.py
@@ -1,19 +1,31 @@
+AppName = "To-do list"
+
 WindowHeight = 500
-WindowWidth = 700
+WindowWidth = 800
 ExtendedWindowHeight = 650
 
 App = {
+    # Length of characters
     'NameMinimumLength': 0,
     'NameMaximumLength': 15,
+
+    # Length of characters
+    'DescriptionMaximumLength': 50,
 }
 
 DisplayErrors = {
     'Login_InvalidPasswordLength': "Password must be more than 8 characters",
     'Login_InvalidUsername': 'This username is already in use',
-    'App_InvalidName': 'You already have a {type} named "{name}"',
-    'App_InvalidNameLength': '''{name} must be between \n%s-%s characters'''%(App['NameMinimumLength'], App['NameMaximumLength']),
-    'App_InvalidTaskInputLength': 'Task must have a name and description',
-    'App_InvalidInput': '{name} cannot contain any symbols'
+    'App_InvalidName': 'You already have a {type} named "{msg}"',
+
+    'App_InvalidNameLength': ('''{msg} must be between \n%s-%s characters'''
+        %(App['NameMinimumLength'], App['NameMaximumLength'])),
+
+    'App_InvalidDescriptionLength': ('''{msg} cannot exceed %s characters'''
+        %(App['DescriptionMaximumLength'])),
+
+    'App_InvalidTaskInput': 'Task name is a required field',
+    'App_InvalidInput': '{msg} cannot contain any symbols'
 }
 
 BaseFont = "Arial"
