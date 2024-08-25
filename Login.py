1a0c1b0fe936f226c50311cff10a1c36934a48dc
stevensonb1 <139927019+stevensonb1@users.noreply.github.com>
2024-08-25 20:36:03 +1200

    window root update

--- Login.py
+++ Login.py
@@ -1,7 +1,7 @@
 import customtkinter
 import keyring
 import Constants
-from App import App as app
+from App import App
 
 # Default
 customtkinter.set_appearance_mode("dark")
@@ -15,21 +15,25 @@ class Login(customtkinter.CTk):
     DEFAULT_TEXT_COLOUR = ("black", "grey")
     DEFAULT_BORDER_COLOUR = "gray37"
 
-    def __init__(self):
+    def __init__(self, main):
         super().__init__()
-        
-        self.title("Login")
-        self.geometry(f"{Constants.WindowWidth}x{Constants.WindowHeight}")
-        self.resizable(width=False, height=False)
+        from Main import Main
 
-        self.main_frame = customtkinter.CTkFrame(self, width=300, height=350)
+        self.main = main
+
+        main.adjust_window_geometry(extended=False)
+       
+        self.main_frame = customtkinter.CTkFrame(main.root, 
+            width=300, height=350)
         self.main_frame.place(relx=0.5,rely=0.5,anchor=customtkinter.CENTER)
         self.main_frame.pack_propagate(False)
 
-        self.title_label = customtkinter.CTkLabel(self.main_frame, text="Login to Account", font=("",20))
+        self.title_label = customtkinter.CTkLabel(self.main_frame, 
+            text="Login to Account", font=("",20))
         self.title_label.pack(pady=50)
         
-        self.content_frame = customtkinter.CTkFrame(self.main_frame, fg_color="transparent")
+        self.content_frame = customtkinter.CTkFrame(self.main_frame, 
+            fg_color="transparent")
         self.content_frame.place(relx=0.5,rely=0.5,anchor=customtkinter.CENTER)
 
         self.load_login_menu()
@@ -37,8 +41,11 @@ class Login(customtkinter.CTk):
     def display_status_error(self, status_error: str):
         if not status_error in Constants.DisplayErrors:
             raise Exception(f'No status error for {status_error}')    
+        
         self.status_error = customtkinter.CTkLabel(self.main_frame, 
-            text=Constants.DisplayErrors[status_error], text_color=self.LIGHT_RED_COLOUR)
+            text=Constants.DisplayErrors[status_error], 
+            text_color=self.LIGHT_RED_COLOUR)
+        
         self.status_error.after(2000, self.status_error.destroy)
         self.status_error.pack(pady=100)
     
@@ -53,10 +60,12 @@ class Login(customtkinter.CTk):
     def auth_login_credentials(self):
         username = self.login_username.get()
         password = self.login_password.get()
+
         if keyring.get_password(service_id, username) == password:
             self.destroy()
-            app(username=username).run()
+            App(main=self.main, username=username)
             return
+        
         self.display_auth_error(self.login_username)
         self.display_auth_error(self.login_password)
 
@@ -66,19 +75,24 @@ class Login(customtkinter.CTk):
         if len(username) == 0 or len(password) < 8:
             self.display_auth_error(self.account_username)
             self.display_auth_error(self.account_password)
+
             if len(password) < 8:
                 self.display_status_error('Login_InvalidPasswordLength')
-        else:
-            self.account_username.delete(0, customtkinter.END)
-            self.account_password.delete(0, customtkinter.END)
-            self.reset_auth(self.account_username)
-            self.reset_auth(self.account_password)
-            if keyring.get_password(service_id, username) != None:
-                self.display_status_error('Login_InvalidUsername')
-            else:
-                keyring.set_password(service_id, username, password)
-                self.destroy()
-                app(username=username).run()
+            return
+        
+        self.account_username.delete(0, customtkinter.END)
+        self.account_password.delete(0, customtkinter.END)
+
+        self.reset_auth(self.account_username)
+        self.reset_auth(self.account_password)
+
+        if keyring.get_password(service_id, username) != None:
+            self.display_status_error('Login_InvalidUsername')
+            return
+        
+        keyring.set_password(service_id, username, password)
+        self.destroy()
+        App(main=self.main, username=username)
         
     def delete_current_page(self):
         [widget.destroy() for widget in self.content_frame.winfo_children()]
@@ -89,17 +103,20 @@ class Login(customtkinter.CTk):
     def load_login_menu(self):
         self.delete_current_page()
         self.change_title_text('Login to Account')
+
         self.login_username = customtkinter.CTkEntry(self.content_frame,
             placeholder_text="Username",
             font=("Helvetica", 14),
         )
         self.login_username.pack(pady=10)
+
         self.login_password = customtkinter.CTkEntry(self.content_frame,
             placeholder_text="Password",
             font=("Helvetica", 14),
             show="*",                              
         )
         self.login_password.pack()
+
         customtkinter.CTkButton(self.content_frame, text="Submit",
             command=self.auth_login_credentials).pack(pady=10)
         customtkinter.CTkButton(self.content_frame,
@@ -110,23 +127,24 @@ class Login(customtkinter.CTk):
     def load_create_account_menu(self):
         self.delete_current_page()
         self.change_title_text('Create an Account')
+
         self.account_username = customtkinter.CTkEntry(self.content_frame,
             placeholder_text="Enter Username",
             font=("Helvetica", 14),                            
         )
         self.account_username.pack(pady=10)
+
         self.account_password = customtkinter.CTkEntry(self.content_frame,
             placeholder_text="Enter Password",
             font=("Helvetica", 14),
             show="*"       
         )
         self.account_password.pack()
+
         customtkinter.CTkButton(self.content_frame, text="Register", 
             command=self.auth_account_credentials).pack(pady=10)
+        
         customtkinter.CTkButton(self.content_frame, 
             hover_color=self.LIGHT_GREY_COLOUR, fg_color="transparent", 
-            text="Login Account", font=('',15,'underline'), command=self.load_login_menu).pack()
-        
-if __name__ == "__main__":
-    login = Login()
-    login.mainloop()
+            text="Login Account", font=('',15,'underline'), 
+            command=self.load_login_menu).pack()
