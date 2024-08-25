5af7713da78514103609de8ece6fd1b3f1620a46
stevensonb1 <139927019+stevensonb1@users.noreply.github.com>
2024-08-25 21:33:29 +1200

    login

--- Login.py
+++ Login.py
@@ -10,12 +10,19 @@ customtkinter.set_default_color_theme("green")
 service_id = "version_1"
 
 class Login(customtkinter.CTk):
+    """
+    The login class represents a login window using customtkinter for GUI.
+    It handles user authentication and account creation.
+    """
     LIGHT_RED_COLOUR = "#f54c4c"
     LIGHT_GREY_COLOUR = "#383837"
     DEFAULT_TEXT_COLOUR = ("black", "grey")
     DEFAULT_BORDER_COLOUR = "gray37"
 
     def __init__(self, main):
+        """
+        Initialises the login window.
+        """
         super().__init__()
         from Main import Main
 
@@ -39,6 +46,9 @@ class Login(customtkinter.CTk):
         self.load_login_menu()
 
     def display_status_error(self, status_error: str):
+        """
+        Displays an error message on the login window
+        """
         if not status_error in Constants.DisplayErrors:
             raise Exception(f'No status error for {status_error}')    
         
@@ -50,14 +60,24 @@ class Login(customtkinter.CTk):
         self.status_error.pack(pady=100)
     
     def display_auth_error(self, widget):
+        """
+        Displays an authenticiation error by highlighting the widget in red.
+        """
         widget.configure(placeholder_text_color=self.LIGHT_RED_COLOUR,
             border_color=self.LIGHT_RED_COLOUR)
 
     def reset_auth(self, widget):
+        """
+        Resets the appearance of the authentication widget to default.
+        """
         widget.configure(placeholder_text_color=self.DEFAULT_TEXT_COLOUR,
             border_color=self.DEFAULT_BORDER_COLOUR)
 
     def auth_login_credentials(self):
+        """
+        Authenticates user login credentials. If the credentials are correct,
+        it opens the main app window. Otherwise, it displays an error.
+        """
         username = self.login_username.get()
         password = self.login_password.get()
 
@@ -70,6 +90,11 @@ class Login(customtkinter.CTk):
         self.display_auth_error(self.login_password)
 
     def auth_account_credentials(self):
+        """
+        Validates and creates a new user account. If credentials are valid,
+        it saves them and opens the main app window. Otherwise, it displays
+        an error or prompts for correction.
+        """
         username = self.account_username.get()
         password = self.account_password.get()
         if len(username) == 0 or len(password) < 8:
@@ -95,12 +120,22 @@ class Login(customtkinter.CTk):
         App(main=self.main, username=username)
         
     def delete_current_page(self):
+        """
+        Removes all widgets from the content frame
+        """
         [widget.destroy() for widget in self.content_frame.winfo_children()]
             
     def change_title_text(self, str):
+        """
+        Changes the text of title label.
+        """
         self.title_label.configure(text=str)
 
     def load_login_menu(self):
+        """
+        Loads the login menu by creating and placing login widgets
+        on the content frame.
+        """
         self.delete_current_page()
         self.change_title_text('Login to Account')
 
@@ -125,6 +160,10 @@ class Login(customtkinter.CTk):
             command=self.load_create_account_menu).pack()
 
     def load_create_account_menu(self):
+        """
+        Loads the account creation menu by creating and placing account
+        creation widgets on the content frame.
+        """
         self.delete_current_page()
         self.change_title_text('Create an Account')
 
